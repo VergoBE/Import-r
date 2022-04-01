9ebe26104fc8e0fa3f87f64f02238829bb943f55
Mads Christian Bechmann Sørensen <mads92n1@edu.easj.dk>
2022-04-01 10:16:25 +0200

    Added Create Car HTML/CS + Update Car method in services/Interface

---
+++ Pages/Car/CreateCar.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Importør.Services;
+using Importør.Models;
+
+namespace Importør.Pages.Car
+{
+    public class CreateCarModel : PageModel
+    {
+        private CarService carService;
+        private List<Models.Car> carList;
+
+        public CreateCarModel(CarService carService)
+        {
+            this.carService = carService;
+            carList = carService.GetCars().ToList();
+        }
+
+        public Models.Car Car { get; set; }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            carService.AddCar(Car);
+            return RedirectToPage("GetAllCars");
+        }
+
+    }
+}
