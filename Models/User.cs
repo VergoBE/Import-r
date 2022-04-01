58ee9204124cb2468ccb1483acb3ddf8ef794967
Emil Eltzholtz Bosse Krasilnikoff <emil02x3@edu.zealand.dk>
2022-04-01 10:13:33 +0200

    User ID + Required + Pathing (GetAllCars)

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace Importør.Models
 {
     public class User
     {
+        [Key, Required]
+        public int UserId { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
         private string Password { get; set; }
         public string Email { get; set; }
 
