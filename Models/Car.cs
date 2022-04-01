3a3bde0b69465b88eefb8a0fa9c4736cb2a0a0b7
RazorSDU <j_hoppe.r@hotmail.com>
2022-04-01 12:50:04 +0200

    Start for migration

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,8 @@ namespace Importør.Models
 {
     public class Car
     {
-        [Key, Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CarId { get; set; }
         [Required]
         public string Type { get; set; }
